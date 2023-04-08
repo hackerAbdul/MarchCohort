2621e45304204b3801ae8f17d45e7beff6794fc1
hackerAbdul <abdulkadir.q12@gmail.com>
2023-04-08 14:13:16 +0100

    updating github with new folders

---
+++ lesson/javascript/lesson1.js
@@ -0,0 +1,32 @@
+/* This 
+is a
+multi-line
+comment */
+
+
+//This is a single line comment
+
+
+//how to print off a string
+console.log("Hello welcome to lesson 1");
+
+//how to print off a number
+//console.log(12);
+
+
+//variables - are jars/containers that hold content for us
+//var 
+//let
+//const
+
+let age = 25;
+const name = "Abdulkadir"
+
+
+console.log(name);
+
+//string concatenation
+console.log("Hello my name is " + name + " and I am " + age + " years old")
+
+//string interpolation
+console.log(`Hello my name is ${name} and I am ${age} years old`)
